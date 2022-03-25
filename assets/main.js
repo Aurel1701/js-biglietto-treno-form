2614aaee20cec72c627bcc5356d4f8b66ac57395
Aurel1701 <gantzfan96@gmail.com>
2022-03-25 18:20:04 +0100

    edit

--- assets/main.js
+++ assets/main.js
@@ -21,14 +21,33 @@ function clickbutton(){
         ticket = ticket - offerta;
         offerta2 = 'sale over65'
     }
-    document.querySelector('.risultato').innerHTML =  `<div class="row">
-    <h4 class="text-center">DETTAGLIO PASSEGGERI</h4>
-    <div class="col nome-passeggero">${firstname}</div>
-    <div class="col Offerta">${offerta2}</div>
-    <div class="col Carrozza">${carrozza}</div>
-    <div class="col Codice-Cp">${cp}</div>
-    <div class="col Costo-biglietto">${ticket.toFixed(2)}$</div>
+    document.querySelector('.risultato').innerHTML =  ` <div class="container">
+    <div class="row  biglietto">
+       <div>
+       <h4>DETTAGLIO PASSEGGERI</h4>
+      </div>
+      <div class="col">
+       <h5>NOME PASSEGGERO</h5>
+       <div class="col passeggero">${firstname}</div>
+      </div>
+      <div class="col">
+       <h5>Offerta</h5>
+       <div class="col Offerta">${offerta2}</div>
+      </div>
+      <div class="col">
+       <h5>Carrozza</h5>
+       <div class="col Carrozza">${carrozza}</div>
+      </div>
+      <div class="col">
+       <h5>Codice Cp</h5>
+       <div class="col Codice-Cp">${cp}</div>
+      </div>
+      <div class="col">
+       <h5>Costo biglietto</h5>
+       <div class="col Costo-biglietto">${ticket.toFixed(2)}$</div>
+      </div>
     </div>
+   </div>
     `
 
 }
