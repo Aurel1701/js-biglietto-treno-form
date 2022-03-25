f34b9b7023817a5def93820bb7776dee729201e9
Aurel1701 <gantzfan96@gmail.com>
2022-03-25 17:16:30 +0100

    edit

--- assets/main.js
+++ assets/main.js
@@ -5,14 +5,28 @@ function clickbutton(){
     let firstname = document.querySelector('#btn1').value;
     let km = document.querySelector('#btn2').value;
     let age = document.querySelector('#age').value;
-    var ticket = km * 0.21;
-    if (age < 18) {
-        ticket = ticket - (ticket * 20 / 100);
+    let ticket = km * 0.21;
+    let carrozza = Math.random();
+    let cp = Math.random();
+    let offerta;
+
+    if (age == 'under18') {
+        offerta = (ticket * 20 / 100)
+        ticket = ticket - offerta;
         
-    } else if (age > 65) {
-        ticket = ticket - (ticket * 40 / 100);
+    } else if (age == 'over65') {
+        offerta = (ticket * 40 / 100)
+        ticket = ticket - offerta;
     }
-    document.querySelector('.risultato').append(`${ticket}`)
+    document.querySelector('.risultato').InsertAdjacentHTML('afterend',`<div class="row">
+    <h4 class="text-center">DETTAGLIO PASSEGGERI</h4>
+    <div class="col nome-passeggero">${firstname}</div>
+    <div class="col Offerta">${offerta}</div>
+    <div class="col Carrozza">${carrozza}</div>
+    <div class="col Codice-Cp">${cp}</div>
+    <div class="col Costo-biglietto">${carrozza}</div>
+    </div>
+    `)
 
 }
 
